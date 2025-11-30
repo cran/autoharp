cf0b93aff7e4ad3d24b518b8ca104e217d0997bc
Agrawal Naman <naman.a@nus.edu.sg>
2025-11-30 22:30:43 +0000

    version 0.0.13

--- R/env_size.R
+++ R/env_size.R
@@ -1,6 +1,6 @@
 #' Calculates the Total Memory Used
 #'
-#' This function uses object_size from the pryr package to compute the total
+#' This function uses the utils package to compute the total
 #' amount of memory used by objects in an environment.
 #'
 #' @param env The environment whose size is to be computed.
@@ -11,36 +11,17 @@
 #' names will cause problems. This function is used within
 #' \code{\link{render_one}} as part of the runtime stats assessment.
 #'
-#' @seealso \code{\link[pryr]{object_size}}
 #' @examples
 #'
 #' e1 <- new.env()
 #' env_size(e1)
 #' evalq(x <- 1:10000L, e1)
 #' env_size(e1)
-env_size <- function(env) {
-  #if(!requireNamespace("pryr", quietly=TRUE)) {
-  #  stop("Please install pryr before using this function. env_size() requires
-  #       it.")
-  #}
-  size_byte <- pryr::object_size(mget(ls(envir = env), envir = env))
-  as.numeric(size_byte)
-#  obj_names <- ls(envir = env)
 
-#  #obj_size_expr <- paste0("pryr::object_size(",
-#  #                        paste(obj_names, collapse=","),
-#  #                        ")")
-#  if(length(obj_names) == 0L) {
-#    obj_size_expr <- paste0("pryr::object_size(",
-#                           paste(obj_names, collapse=","),
-#                           ")")
-#  } else {
-#    obj_size_expr <- paste0("pryr::object_size(",
-#                            paste0("`",obj_names, "`", collapse=","),
-#                            ")")
-#  }
-#  size_bytes <- eval(str2lang(obj_size_expr), envir = env)
-#  #tmp <- 1:1000L
-#  #size_bytes <- object_size(tmp)
-#  as.numeric(size_bytes)
+env_size <- function(env) {
+  obj_names <- ls(envir = env, all.names = TRUE)
+  if (length(obj_names) == 0L) return(0)
+  objs <- mget(obj_names, envir = env, inherits = FALSE)
+  size_byte <- sum(vapply(objs, utils::object.size, numeric(1))) 
+  size_byte
 }
